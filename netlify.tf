7f3a199a8f135946049cc6c7168c3c6194fcb213
Pi Delport <pjdelport@gmail.com>
2019-04-03 12:38:12 +0200

    Import Netlify site: affidavit-evictions-org

--- netlify.tf
+++ netlify.tf
@@ -6,3 +6,16 @@
 provider "netlify" {
   token = "${var.netlify_token}"
 }
+
+# Imported using: terraform import netlify_site.affidavit-evictions-org 4c617890-48dd-422d-bbdb-d1361ee3e96d
+resource "netlify_site" "affidavit-evictions-org" {
+  name = "affidavit-evictions-org"
+
+  repo {
+    provider    = "github"
+    repo_path   = "OpenUpSA/eviction-resources"
+    repo_branch = "master"
+    command     = "gatsby build"
+    dir         = "public/"
+  }
+}
